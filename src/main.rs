7c9cf8e436366719718491fe5fe0624aa8e6968a
Matthew Kennedy <burnsidemk@gmail.com>
2018-01-14 22:24:22 -0600

    Some stuff

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,89 @@ struct Vertex {
     z: f32,
 }
 
+impl std::ops::Sub for Vertex {
+    type Output = Vertex;
+
+    fn sub(self, rhs: Vertex) -> Self::Output {
+        Vertex { x: self.x - rhs.x, y: self.y - rhs.y, z: self.z - rhs.z }
+    }
+}
+
+impl std::ops::Add for Vertex {
+    type Output = Vertex;
+
+    fn add(self, rhs: Vertex) -> Self::Output {
+        Vertex { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+    }
+}
+
+impl Vertex {
+    fn scale(&self, n: f32) -> Vertex {
+        Vertex { x: self.x * n, y: self.y * n, z: self.z * n }
+    }
+
+    fn dot(&self, rhs: Vertex) -> f32 {
+        self.x * rhs.x + self.y * rhs.y + self.z + rhs.z
+    }
+
+    fn cross(&self, rhs: Vertex) -> Vertex {
+        Vertex { x: self.y * rhs.z - self.z * rhs.y, y: self.z * rhs.x - self.x * rhs.z, z: self.x * rhs.y - self.y * rhs.x }
+    }
+
+    fn length(&self) -> f32 {
+        (self.x * self.x + self.y * self.y + self.z * self.z).sqrt()
+    }
+
+    fn unit(&self) -> Vertex {
+        self.scale(1.0 / self.length())
+    }
+}
+
+struct Vertices {
+    vertices: Vec<Vertex>
+}
+
+impl Iterator for Vertices {
+    type Item = ();
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unimplemented!()
+    }
+}
+impl Vertices {
+    fn max_length(&self) -> Option<f32> {
+        self.vertices.iter()
+            .map(Vertex::length)
+            .max_by(|a, b| a.partial_cmp(b).unwrap())
+    }
+}
+
+struct Triangle {
+    a: Vertex,
+    b: Vertex,
+    c: Vertex,
+}
+
+impl Triangle {
+    fn unit(&self) -> Triangle {
+        Triangle { a: self.a.unit(), b: self.b.unit(), c: self.c.unit() }
+    }
+
+    fn scale(&self, n: f32) -> Triangle {
+        Triangle { a: self.a.scale(n), b: self.b.scale(n), c: self.c.scale(n) }
+    }
+}
+
+struct Triangles {
+    triangles: Vec<Triangle>
+}
+
 impl Vertex {
     fn parse3(x: &str, y: &str, z: &str) -> Vertex {
         Vertex { x: x.parse().unwrap(), y: y.parse().unwrap(), z: z.parse().unwrap() }
     }
     fn parse2(x: &str, y: &str) -> Vertex {
-        Vertex { x: x.parse().unwrap(), y: y.parse().unwrap(), z: 0.0f32 }
+        Vertex { x: x.parse().unwrap(), y: y.parse().unwrap(), z: 0.0 }
     }
 }
 
@@ -30,6 +107,7 @@ impl Face {
         fn helper(triple: &str) -> (u16, u16, u16) {
             let triples: Vec<&str> = triple.split("/").collect();
             match triples.as_slice() {
+                // FIXME need to subtract 1 from each here
                 &[a, b, c] => (a.parse().unwrap(), b.parse().unwrap(), c.parse().unwrap()),
                 _ => unreachable!()
             }
@@ -56,16 +134,12 @@ impl Obj {
             let line = line.unwrap();
             let fields: Vec<&str> = line.split_whitespace().collect();
             match fields.as_slice() {
-                &["vn", x, y, z] =>
-                    vsn.push(Vertex::parse3(x, y, z)),
-                &["vt", x, y, z] =>
-                    vst.push(Vertex::parse3(x, y, z)),
-                &["vt", x, y] =>
-                    vst.push(Vertex::parse2(x, y)),
-                &["v", x, y, z] =>
-                    vsv.push(Vertex::parse3(x, y, z)),
+                &["vn", x, y, z] => vsn.push(Vertex::parse3(x, y, z)),
+                &["vt", x, y, z] => vst.push(Vertex::parse3(x, y, z)),
+                &["vt", x, y] => vst.push(Vertex::parse2(x, y)),
+                &["v", x, y, z] => vsv.push(Vertex::parse3(x, y, z)),
                 &["f", v, t, n] => fs.push(Face::parse(v, t, n)),
-                &["#", ..] => {}
+                &["#", ..] | &[] => {}
                 _ => unreachable!()
             }
         }
@@ -75,8 +149,16 @@ impl Obj {
 
 #[test]
 fn test() {
-    let file = File::open("model/anju.obj").unwrap();
-    Obj::parse(file);
+    for file in vec!["model/anju.obj", "model/charizard.obj"] {
+        let file = File::open(file).unwrap();
+        Obj::parse(file);
+    }
+}
+
+#[test]
+fn test_max_length() {
+    println!("{:?}", Vertices { vertices: vec![Vertex { x: 1.0, y: 1.0, z: 2.0 }] }.max_length());
+    println!("{:?}", Vertices { vertices: vec![] }.max_length())
 }
 
 fn main() {
